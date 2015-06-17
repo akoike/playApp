f3fd57a9428e265d34ed7ec6ee66a368c6edcb35
Your Name <you@example.com>
2015-06-18 01:12:28 +0900

    second

--- app/models/Test.java
+++ app/models/Test.java
@@ -4,6 +4,6 @@ package models;
  * Created by akoike on 2015/06/18.
  */
 public class Test {
-        public String name = "小池篤史";
+        public String name = "name";
         public int age = 29;
 }
