8c4d3f169d0d41deb8a56c2b38fc6a59aeef05c5
Your Name <you@example.com>
2015-06-22 23:34:19 +0900

    sqlテスト

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,20 @@
 package controllers;
 
+import com.avaje.ebean.Query;
+import models.entity.User;
 import play.*;
 import play.db.ebean.Model;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        Query<User> user = User.find.where("age='30'");
+        return ok(index.render(user.findUnique()));
     }
-
-
 
 }
--- app/models/entity/User.java
+++ app/models/entity/User.java
@@ -1,21 +1,26 @@
 package models.entity;
 
 import com.avaje.ebean.annotation.CreatedTimestamp;
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Version;
+import javax.validation.Constraint;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
  * Created by akoike on 2015/06/20.
  */
+@Entity
 public class User extends Model {
 
     @Id
     public long id;
 
+    @Constraints.Required
     @NotNull
     public String name;
 
@@ -31,6 +36,8 @@ public class User extends Model {
     @Version
     public Date updateDate;
 
+    public static Finder<Long, User> find = new Finder<Long, User>(Long.class, User.class);
+
     public String toString() {
         return "User [" +
                 "id=" + id + ", " +
@@ -42,9 +49,16 @@ public class User extends Model {
                 "]";
     }
 
-    public enum SexType{
-        MAN,
-        WOMAN;
+    public enum SexType {
+        MAN("男"),
+        WOMAN("女");
+        public String sexType;
+        SexType(String sexType) {
+            this.sexType = sexType;
+        }
+        public String getSexType() {
+            return this.sexType;
+        }
     }
 
 }
